1378edf20e829bd2657c5dcd501e3d978dccdb65
leo <mxmleosallier@gmail.com>
2023-02-17 11:42:51 +0100

    TEST dmg

--- forge.config.js
+++ forge.config.js
@@ -30,13 +30,17 @@ module.exports = {
     },
     {
       name: '@electron-forge/maker-deb',
-      config: {},
+      config: {
+        
+
+      },
     },
     {
       name: '@electron-forge/maker-dmg',
       platforms: ['darwin'],
       config: {
-        format: 'ULFO'
+        format: 'ULMO', 
+        debug: true,
       }
     }
   ],
